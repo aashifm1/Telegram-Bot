a369edcd3f40bc94c7069c7b4da5c71d913a64b9
Aashif M <Aashif8056@gmail.com>
2025-03-08 19:36:24 +0530

    Update coursemaster.py

--- coursemaster.py
+++ coursemaster.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-# Bot token - environment variable
+# Bot token - environment variables
 Token = os.getenv("BOT_TOKEN")
 
 if not Token:
@@ -20,42 +20,16 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         """
         /start -> Welcome to the Page
-/help -> It shows available Commands 
-/courses -> It shows available Courses
-/roadmap -> Mastering Programming Language
-/problems -> Practice problems to Solve
-/projects -> It gives real-time projects
-        """
-    )
-
-# Course command
-async def courses(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        """
-        /C -> Gives C course link
+/help -> It executes this message
+/C -> Gives C course link
 /CPP -> Gives C++ course link
 /Python -> Gives Python course link
 /Java -> Gives Java course link
-/Rust -> Gives Rust course link
+/SQL -> Gives SQL course link
         """
     )
 
-
-# Roadmap - Drive link
-async def roadmap(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("https://drive.google.com/drive/folders/1jp6XBJVH_fbItFL3i2Nvhj1xehJNt31_?usp=sharing")
-
-
-# Problems Command
-async def problems(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hey this is a problem. solve it")
-
-# Projects command
-async def projects(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hey this is a project demo")
-
-
-# Course links
+# Course commands
 async def C(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Tutorial link for C: https://www.udemy.com/course/c-programming-for-beginners-/")
 
@@ -68,33 +42,25 @@ async def Python(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def Java(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Tutorial link for Java: https://www.udemy.com/course/java-programming-tutorial-for-beginners/")
 
-async def Rust(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Tutorial link for Rust: https://www.udemy.com/course/rust-lang/")
-
+async def SQL(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Tutorial link for SQL: https://www.udemy.com/course/the-complete-sql-bootcamp/")
 
 # Unknown command handler
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Sorry, I didn't understand that command. Please use /help to see the list of available commands.")
 
-
 # Add handlers
 app.add_handler(CommandHandler('start', start))
 app.add_handler(CommandHandler('help', help))
-app.add_handler(CommandHandler('roadmap', roadmap))
-app.add_handler(CommandHandler('courses', courses))
-app.add_handler(CommandHandler('problems', problems))
-app.add_handler(CommandHandler('projects', projects))
-
 app.add_handler(CommandHandler('C', C))
 app.add_handler(CommandHandler('CPP', CPP))
 app.add_handler(CommandHandler('Python', Python))
 app.add_handler(CommandHandler('Java', Java))
-app.add_handler(CommandHandler('Rust', Rust))
-
+app.add_handler(CommandHandler('SQL', SQL))
 app.add_handler(MessageHandler(filters.COMMAND, unknown))
 
 # Run the bot
 if __name__ == "__main__":
     app.run_polling()
 
-# updated help and added courses, roadmap, problems, projects - 08/03/2025
+# Last edited - 06/03/2025
